77c8517e81a076ae3ac5a7e41d45e87e7bb717c9
Sijie Fu <fusijie@cmu.edu>
2022-12-02 16:42:55 -0500

    main

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import sys, os
 import pickle
-import 
+import json
+import argparse
+from datetime import datetime as dt
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from torchdrug import data, datasets
+from torchdrug import core, models, tasks
+from torchdrug import utils
+
+# Collaborators for this team project
+__author__ = 'Sijie Fu, Nicholas Hattrup, Robert MacKnight'
+__email__ = 'sijief | nhattrup | rmacknig@andrew.cmu.edu'
+__forkedrepo__ = 'https://github.com/DeepGraphLearning/torchdrug'
+
+parser = argparse.ArgumentParser(description='Final team project repo for CMU 10-617 (Fall 2022)')
